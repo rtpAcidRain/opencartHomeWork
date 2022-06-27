7cc2ad9cdf2d11015a8a5eeb492d019250819a2f
Илья Матвеев <matweev.ilja@yandex.ru>
2022-06-27 12:44:09 +0300

    init&home2

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://iljam28o.beget.tech/admin/');
+define('HTTP_CATALOG', 'http://iljam28o.beget.tech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://iljam28o.beget.tech/admin/');
+define('HTTPS_CATALOG', 'http://iljam28o.beget.tech/');
+
+// DIR
+define('DIR_APPLICATION', '/home/i/iljam28o/iljam28o.beget.tech/public_html/admin/');
+define('DIR_SYSTEM', '/home/i/iljam28o/iljam28o.beget.tech/public_html/system/');
+define('DIR_IMAGE', '/home/i/iljam28o/iljam28o.beget.tech/public_html/image/');
+define('DIR_STORAGE', '/home/i/iljam28o/iljam28o.beget.tech/storage/');
+define('DIR_CATALOG', '/home/i/iljam28o/iljam28o.beget.tech/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'iljam28o_oc');
+define('DB_PASSWORD', 'Kwd3*xn%');
+define('DB_DATABASE', 'iljam28o_oc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
